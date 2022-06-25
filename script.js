457f327ad627199e99d9f0d7177066e686347eae
Mario Lisbona <mario.lisbona@gmail.com>
2022-06-25 15:10:55 +1000

    create single expanded ccordian at a time

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-let accordion = document.getElementsByClassName("accordion");
-let i;
+// let accordion = document.getElementsByClassName("accordion");
+// let i;
 
 
-for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px"
-        }
-    });
-}
+// for (i = 0; i < accordion.length; i++) {
+//     accordion[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         let panel = this.nextElementSibling;
+//         if (panel.style.maxHeight) {
+//             panel.style.maxHeight = null;
+//         } else {
+//             panel.style.maxHeight = panel.scrollHeight + "px"
+//         }
+//     });
+// }
+
+
+let accordion = document.querySelectorAll(".accordion");
+let active = null;
+
+accordion.forEach((item, i) => {
+  item.addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    
+    
+    if(active) {
+        active.style.maxHeight = null;
+    }
+    if(panel !== active) {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      active = panel
+     
+    } else {
+      active = null
+    }
+
+  });
+});
+
+
 
 
